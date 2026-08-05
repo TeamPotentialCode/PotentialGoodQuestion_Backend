bd6a309205d96ea1594a056c6a1d24458a2c97e0
todok0317 <todok0317@naver.com>
2026-08-05 22:11:43 +0900

    init : 환경 개발 설정

---
+++ src/main/java/com/potential/goodquestion/GoodquestionApplication.java
@@ -0,0 +1,13 @@
+package com.potential.goodquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodquestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodquestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potential/goodquestion/GoodquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potential.goodquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
