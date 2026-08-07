247e50f6cde948d05cbfbd0992b4c32fbfc77bc3
todok0317 <todok0317@naver.com>
2026-08-07 22:39:07 +0900

    feat: 빌드 의존성, 애플리케이션 설정, 환경변수 템플릿 추가

--- src/main/java/com/potential/goodquestion/GoodquestionApplication.java
+++ src/main/java/com/potential/goodquestion/GoodquestionApplication.java
@@ -2,8 +2,10 @@ package com.potential.goodquestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // BaseEntity의 createdAt / updatedAt 자동 관리
 public class GoodquestionApplication {
 
 	public static void main(String[] args) {
