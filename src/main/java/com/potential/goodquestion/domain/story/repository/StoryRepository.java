b9fc7ac8354b6885e5f8b799e781e28089b6b50c
전우선 <jeonwooseon@naver.com>
2026-08-08 00:51:29 +0900

    fix: topics 필드 변경에 따른 StoryRepository 조회 메서드 수정

--- src/main/java/com/potential/goodquestion/domain/story/repository/StoryRepository.java
+++ src/main/java/com/potential/goodquestion/domain/story/repository/StoryRepository.java
@@ -10,7 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StoryRepository extends JpaRepository<Story, Long> {
 
     /**
-     * 주제로 이야기 목록 조회 (주제별 필터링)
+     * 주제로 이야기 목록 조회 (JSON 배열에 포함된 주제 필터링)
      */
-    List<Story> findAllByTopic(String topic);
+    @org.springframework.data.jpa.repository.Query("SELECT s FROM Story s WHERE s.topics LIKE %:topic%")
+    List<Story> findAllByTopicContaining(@org.springframework.data.repository.query.Param("topic") String topic);
 }
